7d93b112bdcd86074042b2c5d4ba505d6251e914
Michał Grzędzicki <lazy404@gmail.com>
2013-05-01 23:10:09 +0200

    loop thread

--- lds/net/loop.py
+++ lds/net/loop.py
@@ -11,21 +11,53 @@ import threading, Queue
 
 from lds.net.receiver import ReceiverThread
 from lds.net.sender import SenderThread
+from lds.net.proto import *
+from lds.video.omxplayer import OmxPlayerThread
 
 class NetLoopThread(threading.Thread):
-    def __init__(self, addr, port):
+    def __init__(self):
         threading.Thread.__init__(self)
-        self.end=False
-        
+        self.want_end=False
+	self.hfmt='!BBBBBB'
+	self.hlen=struct.calcsize(self.hfmt)
+    
+    def end(self):
+	self.rec.end()
+	self.want_end=True
+
     def run(self):
         rec=ReceiverThread('225.0.0.2', 2000)
         rec.start()
+        send=SenderThread('225.0.0.2', 2000)
+        send.start()
+	
         rq=rec.get_queue()
-    
-        while not self.end:
-            print rq.get()
-            
+	sq=send.get_queue()
+
+	self.rec=rec
+
+        while not self.want_end:
+            data=rq.get()
+
+	    ver, cmdid, cmd, cmdflag, cmdopt , optlen= struct.unpack(self.hfmt, data[0][:self.hlen])
+
+	    if optlen > 0:
+		optstr = struct.unpack_from("%ds" % optlen, data[0], self.hlen)
+	    else:
+		optstr=False
+
+	    if cmd == PREPARE_VIDEO:
+		self.vt=OmxPlayerThread(optstr)
+		self.vt.start()
+		self.vt.wait_ready()
+
+	    if cmd == RUN_VIDEO:
+		self.vt.play_video()
+		self.vt.wait_ready()
+
+
 if __name__ == '__main__':
     s=NetLoopThread()
-    #s.start()
-    s.run()
+    s.start()
+    raw_input('end ?')
+    s.end()
